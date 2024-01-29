e029eed346a561b3606c2dfb61d54f01d9e931a6
Dhara Patel <pateldharapravin@gmail.com>
2024-01-28 19:07:36 -0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <h2>
+        We Hear You
+      </h2>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>    
+      <h3>
+        Be Aware - The Deaf Assistant
+      </h3>
+    </>
+  )
+}
+
+export default App
---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAAHDfunDFhQDBTJeQid93oGKgkS3R8kZs",
+  authDomain: "we-hear-you-5a05a.firebaseapp.com",
+  projectId: "we-hear-you-5a05a",
+  storageBucket: "we-hear-you-5a05a.appspot.com",
+  messagingSenderId: "1070250683398",
+  appId: "1:1070250683398:web:bbc390d3a67870e161a787",
+  measurementId: "G-H88WYW8RFJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+const analytics = getAnalytics(app);
