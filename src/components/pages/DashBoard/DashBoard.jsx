3301b73d4a7ece5f9c76b2e5c03a8e7509a83e2b
darsh <darshwork008@gmail.com>
2024-03-22 10:53:25 -0400

    remove past stream as faving error

--- src/components/pages/DashBoard/DashBoard.jsx
+++ src/components/pages/DashBoard/DashBoard.jsx
@@ -83,23 +83,7 @@ function CurrentStream() {
 }
 
 
-function PastStream() {
-    return (
-        <>
-            <div className="past-stream-container">
-                <div className="past-streams-card">
-                    {/* <div className="past-streams-rounded-square"></div> */}
-                    <div className="past-streams-rectangle"></div>
-                    <div className="past-streams-logo">
-                        <img src="logo.png" alt="Company Logo" />
-                    </div>
-                    <div className="past-streams-company-name">Company Name</div>
-                </div>
-            </div>
-        </>
-    );
-}
-
+// the code had error @karan - please submit the new code or i will update in next commit
 
 
 
